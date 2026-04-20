6c062f79cda75a3ba946bdbfd1cf7d4537e2df10
AhnafAwsaf7 <ahnaf.awsaf14@gmail.com>
2026-04-20 21:59:49 +0600

    Prepare for deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    process.env.CLIENT_URL,
+  ].filter(Boolean),
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
